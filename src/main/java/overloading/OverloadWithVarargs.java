dadfdbde6fac2a09cf97571b65623f73016e83ce
hr-pommala <hr@pommala.com>
2024-11-25 20:53:43 -0600

    Updated VarArgs

--- src/main/java/overloading/OverloadWithVarargs.java
+++ src/main/java/overloading/OverloadWithVarargs.java
@@ -4,12 +4,22 @@ public class OverloadWithVarargs {
 
 	// Method with varargs
     public void display(int... numbers) {
-        System.out.print("Numbers: ");
+        System.out.print("In display with single method arguments \nNumbers: ");
         for (int number : numbers) {
             System.out.print(number + " ");
         }
         System.out.println();
     }
+    
+	// Method with multiple argumets varargs. VarAgrs should be last argument
+    public void display(String st, int... numbers) {
+        
+    	System.out.print("In display with multiple method arguments \nNumbers: ");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+     }
 
     public static void main(String[] args) {
     	
@@ -17,8 +27,9 @@ public class OverloadWithVarargs {
         
         // Calling overloaded methods with different numbers of arguments
         obj.display(1);          // Calls method with one argument
-        obj.display(1, 2, 3);    // Calls method with multiple arguments
+        obj.display(1, 2, 3,4, 5);    // Calls method with multiple arguments
         obj.display();           // Calls method with no arguments
+        obj.display("Hello World", 1, 2, 3);    // Calls method with multiple arguments
     }
     
 }
